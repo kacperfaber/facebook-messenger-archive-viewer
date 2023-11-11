3634c46b622641a3e6b1f5bc47a1d0a0812b5b04
Kacper Faber <kacperf1234@gmail.com>
2023-11-11 15:01:31 +0100

    To db added methods to save attachments:
    - 'save_video_attachment'
    - 'save_gif_attachment'
    - 'save_audio_attachment'

--- image/db.py
+++ image/db.py
@@ -11,6 +11,12 @@ class Db:
         self.session = sessionmaker(bind=self.engine)
         self.session = Session(bind=self.engine)
 
+    def __save_attachment(self, type1: AttachmentType, rel_path: str, data: str) -> AttachmentDto:
+        attachment = AttachmentDto(type1=type1, data=data, path=rel_path)
+        self.session.add(attachment)
+        self.session.commit()
+        return attachment
+
     def save_photo_attachment(self, rel_path: str, data: str) -> int:
         attachment = AttachmentDto(AttachmentType.PHOTO, data, rel_path)
         self.session.add(attachment)
@@ -19,3 +25,13 @@ class Db:
 
     def get_attachment_by_id(self, id1: int) -> AttachmentDto | None:
         return self.session.query(AttachmentDto).filter_by(id=id1).first()
+
+    def save_video_attachment(self, rel_path: str, data: str) -> AttachmentDto:
+        return self.__save_attachment(AttachmentType.VIDEO, rel_path, data)
+
+    def save_gif_attachment(self, rel_path: str, data: str) -> AttachmentDto:
+        return self.__save_attachment(AttachmentType.GIF, rel_path, data)
+
+    def save_audio_attachment(self, rel_path: str, data: str) -> AttachmentDto:
+        return self.__save_attachment(AttachmentType.AUDIO, rel_path, data)
+
