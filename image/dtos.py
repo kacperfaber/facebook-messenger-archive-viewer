a88e74476108fdea484bb2c9ac30dfabef4d4563
Kacper Faber <kacperf1234@gmail.com>
2023-11-12 20:27:54 +0100

    dtos.py are now dataclasses

--- image/dtos.py
+++ image/dtos.py
@@ -1,5 +1,7 @@
+import dataclasses
 import datetime
 from enum import Enum as PythonEnum
+from typing import List
 
 from sqlalchemy import Column, String, Enum, Integer, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
@@ -8,40 +10,21 @@ from sqlalchemy.orm import relationship
 DeclarativeDb = declarative_base()
 
 
-class ThreadType(PythonEnum):
+class ThreadType(str, PythonEnum):
     GROUP = 'group'
     CONVERSATION = 'conversation'
     PENDING = 'pending'  # Generally in message_requests
 
 
-class ThreadLocation(PythonEnum):
+class ThreadLocation(str, PythonEnum):
     ARCHIVED = 'archived'
     INBOX = 'inbox'
     MESSAGE_REQUESTS = 'message_requests'
     FILTERED_THREADS = 'filtered_threads'
 
 
-# noinspection SpellCheckingInspection
-class ThreadDto(DeclarativeDb):
-    __tablename__ = "threads"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    title = Column(String, nullable=False)
-    thread_type = Column(Enum(ThreadType), nullable=False)
-    thread_path = Column(String, nullable=False)
-    rel_path = Column(String, nullable=False)
-    location = Column(Enum(ThreadLocation), nullable=False)
-    messages = relationship("MessageDto")
-
-    def __init__(self, title: str, thread_type: ThreadType, thread_path: str, thread_location: ThreadLocation, rel_path: str):
-        self.title = title
-        self.thread_type = thread_type
-        self.thread_path = thread_path
-        self.location = thread_location
-        self.rel_path = rel_path
-
-
-class MessageType(PythonEnum):
+class MessageType(str, PythonEnum):
     SHARE = "share"  # "username sent a live location"
     CALL = 'call'
     GENERIC = 'generic'
@@ -50,12 +33,13 @@ class MessageType(PythonEnum):
 
 
 # noinspection SpellCheckingInspection
+@dataclasses.dataclass
 class MessageDto(DeclarativeDb):
     __tablename__ = "messages"
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id: int = Column(Integer, primary_key=True, autoincrement=True)
 
     # Relation with Thread
-    thread_id = Column(Integer, ForeignKey("threads.id"))
+    thread_id: int = Column(Integer, ForeignKey("threads.id"))
     thread = relationship("ThreadDto", back_populates="messages")
 
     sender_name = Column(String, nullable=False)
@@ -66,20 +50,21 @@ class MessageDto(DeclarativeDb):
     attachments = relationship("MessageAttachmentDto", back_populates="message")
 
 
-class AttachmentType(PythonEnum):
+class AttachmentType(str, PythonEnum):
     PHOTO = 'photo'
-    VIDEO = 'video'
-    GIF = 'gif'
-    AUDIO = 'audio'
+    VIDEO: str = 'video'
+    GIF: str = 'gif'
+    AUDIO: str = 'audio'
 
 
 # noinspection SpellCheckingInspection
+@dataclasses.dataclass
 class AttachmentDto(DeclarativeDb):
     __tablename__ = "attachments"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    type = Column(Enum(AttachmentType), nullable=False)
-    data = Column(String, nullable=False)
-    path = Column(String, nullable=False)
+    id: int = Column(Integer, primary_key=True, autoincrement=True)
+    type: AttachmentType = Column(Enum(AttachmentType), nullable=False)
+    data: str = Column(String, nullable=False)
+    path: str = Column(String, nullable=False)
 
     def __init__(self, type1: AttachmentType, data: str, path: str):
         self.type = type1
@@ -87,23 +72,44 @@ class AttachmentDto(DeclarativeDb):
         self.path = path
 
 
-class MessageAttachmentType(PythonEnum):
+class MessageAttachmentType(str, PythonEnum):
     PHOTO = 'photo'
     VIDEO = 'video'
     GIF = 'gif'
     AUDIO = 'audio'
 
+# noinspection SpellCheckingInspection
+@dataclasses.dataclass
+class ThreadDto(DeclarativeDb):
+    __tablename__ = "threads"
+
+    id: int = Column(Integer, primary_key=True, autoincrement=True)
+    title: str = Column(String, nullable=False)
+    thread_type: str = Column(Enum(ThreadType), nullable=False)
+    thread_path: str = Column(String, nullable=False)
+    rel_path: str = Column(String, nullable=False)
+    location: str = Column(Enum(ThreadLocation), nullable=False)
+    messages: List[MessageDto] = relationship("MessageDto")
+
+    def __init__(self, title: str, thread_type: ThreadType, thread_path: str, thread_location: ThreadLocation, rel_path: str):
+        self.title = title
+        self.thread_type = thread_type
+        self.thread_path = thread_path
+        self.location = thread_location
+        self.rel_path = rel_path
+
 
 # noinspection SpellCheckingInspection
+@dataclasses.dataclass
 class MessageAttachmentDto(DeclarativeDb):
     __tablename__ = "message_attachments"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    message_id = Column(Integer, ForeignKey("messages.id"))
-    message = relationship("MessageDto", back_populates="attachments")
-    uri = Column(String, nullable=False)
-    thumbnail_uri = Column(String, nullable=True)
-    created_at = Column(DateTime, nullable=True)
-    type = Column(Enum(MessageAttachmentType), nullable=False)
+    id: int = Column(Integer, primary_key=True, autoincrement=True)
+    message_id: int = Column(Integer, ForeignKey("messages.id"))
+    message: MessageDto = relationship("MessageDto", back_populates="attachments")
+    uri: str = Column(String, nullable=False)
+    thumbnail_uri: str = Column(String, nullable=True)
+    created_at: datetime.datetime = Column(DateTime, nullable=True)
+    type: MessageAttachmentType = Column(Enum(MessageAttachmentType), nullable=False)
 
     def __init__(self, uri: str, type1: MessageAttachmentType, thumbnail_uri: str | None = None, created_at: datetime.datetime | None = None):
         self.uri = uri
