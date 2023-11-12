69231342c23eccfa6c70c93ed7732f8fac7e42a2
Kacper Faber <kacperf1234@gmail.com>
2023-11-12 21:33:11 +0100

    controller_utils - created 'get_page_args'
    get_page_args read parameters 'total' or 'page' or defaults

--- api/routes/controller_utils.py
+++ api/routes/controller_utils.py
@@ -1,5 +1,7 @@
 import dataclasses
 
+from flask import request
+
 
 def opt_json(v):
     """
@@ -11,3 +13,14 @@ def opt_json(v):
 
     else:
         return '', 400
+
+
+def get_page_args(def_total=10, def_page=0) -> (int, int):
+    """
+    :param def_total: Default 'total'
+    :param def_page: Default 'page'
+    :return: (total: int, page: int)
+    """
+    total = request.args.get("total", default=def_total, type=int)
+    page = request.args.get("page", default=def_page, type=int)
+    return total, page
