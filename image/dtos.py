45f52e37dd643d8e0661344b7624b14babbe6212
Kacper Faber <kacperf1234@gmail.com>
2023-11-13 01:28:48 +0100

    dtos.py: Added type annotations to MessageDto fields

--- image/dtos.py
+++ image/dtos.py
@@ -5,7 +5,7 @@ from typing import List
 
 from sqlalchemy import Column, String, Enum, Integer, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped
 
 DeclarativeDb = declarative_base()
 
@@ -23,13 +23,39 @@ class ThreadLocation(str, PythonEnum):
     FILTERED_THREADS = 'filtered_threads'
 
 
-
 class MessageType(str, PythonEnum):
     SHARE = "share"  # "username sent a live location"
     CALL = 'call'
     GENERIC = 'generic'
     SUBCRIBE = 'subscribe'  # "username1 added username2 to the group."
     UNSUBCRIBE = 'unsubscribe'  # "You left the group." / "username1 left the group"
+
+
+class MessageAttachmentType(str, PythonEnum):
+    PHOTO = 'photo'
+    VIDEO = 'video'
+    GIF = 'gif'
+    AUDIO = 'audio'
+
+
+# noinspection SpellCheckingInspection
+@dataclasses.dataclass
+class MessageAttachmentDto(DeclarativeDb):
+    __tablename__ = "message_attachments"
+    id: int = Column(Integer, primary_key=True, autoincrement=True)
+    message_id: int = Column(Integer, ForeignKey("messages.id"))
+    message = relationship("MessageDto", back_populates="attachments")
+    uri: str = Column(String, nullable=False)
+    thumbnail_uri: str = Column(String, nullable=True)
+    created_at: datetime.datetime = Column(DateTime, nullable=True)
+    type: MessageAttachmentType = Column(Enum(MessageAttachmentType), nullable=False)
+
+    def __init__(self, uri: str, type1: MessageAttachmentType, thumbnail_uri: str | None = None,
+                 created_at: datetime.datetime | None = None):
+        self.uri = uri
+        self.thumbnail_uri = thumbnail_uri
+        self.created_at = created_at
+        self.type = type1
 
 
 # noinspection SpellCheckingInspection
@@ -42,12 +68,12 @@ class MessageDto(DeclarativeDb):
     thread_id: int = Column(Integer, ForeignKey("threads.id"))
     thread = relationship("ThreadDto", back_populates="messages")
 
-    sender_name = Column(String, nullable=False)
-    sent_at = Column(DateTime, nullable=False)
-    content = Column(String, nullable=True)
-    type = Column(Enum(MessageType), nullable=False)
-    call_duration = Column(Integer, nullable=True)
-    attachments = relationship("MessageAttachmentDto", back_populates="message")
+    sender_name: str = Column(String, nullable=False)
+    sent_at: datetime.datetime = Column(DateTime, nullable=False)
+    content: str = Column(String, nullable=True)
+    type: MessageType = Column(Enum(MessageType), nullable=False)
+    call_duration: int = Column(Integer, nullable=True)
+    attachments: Mapped[List[MessageAttachmentDto]] = relationship("MessageAttachmentDto", back_populates="message")
 
 
 class AttachmentType(str, PythonEnum):
@@ -72,12 +98,6 @@ class AttachmentDto(DeclarativeDb):
         self.path = path
 
 
-class MessageAttachmentType(str, PythonEnum):
-    PHOTO = 'photo'
-    VIDEO = 'video'
-    GIF = 'gif'
-    AUDIO = 'audio'
-
 # noinspection SpellCheckingInspection
 @dataclasses.dataclass
 class ThreadDto(DeclarativeDb):
@@ -91,28 +111,10 @@ class ThreadDto(DeclarativeDb):
     location: str = Column(Enum(ThreadLocation), nullable=False)
     messages = relationship("MessageDto")
 
-    def __init__(self, title: str, thread_type: ThreadType, thread_path: str, thread_location: ThreadLocation, rel_path: str):
+    def __init__(self, title: str, thread_type: ThreadType, thread_path: str, thread_location: ThreadLocation,
+                 rel_path: str):
         self.title = title
         self.thread_type = thread_type
         self.thread_path = thread_path
         self.location = thread_location
         self.rel_path = rel_path
-
-
-# noinspection SpellCheckingInspection
-@dataclasses.dataclass
-class MessageAttachmentDto(DeclarativeDb):
-    __tablename__ = "message_attachments"
-    id: int = Column(Integer, primary_key=True, autoincrement=True)
-    message_id: int = Column(Integer, ForeignKey("messages.id"))
-    message = relationship("MessageDto", back_populates="attachments")
-    uri: str = Column(String, nullable=False)
-    thumbnail_uri: str = Column(String, nullable=True)
-    created_at: datetime.datetime = Column(DateTime, nullable=True)
-    type: MessageAttachmentType = Column(Enum(MessageAttachmentType), nullable=False)
-
-    def __init__(self, uri: str, type1: MessageAttachmentType, thumbnail_uri: str | None = None, created_at: datetime.datetime | None = None):
-        self.uri = uri
-        self.thumbnail_uri = thumbnail_uri
-        self.created_at = created_at
-        self.type = type1
